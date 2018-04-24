938073292542623c67a59683ee006ff4ff493196
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-23 19:19:16 -0500

    modified route names slightly

--- app/routing/routes.js
+++ app/routing/routes.js
@@ -3,8 +3,8 @@ var path = require('path');
 
 var routes = express.Router();
 
-routes.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
-routes.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '../public/table.html')));
+routes.get('', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+routes.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '../public/tables.html')));
 routes.get('/reservations', (req, res) => res.sendFile(path.join(__dirname, '../public/reservation.html')));
 
 module.exports = routes;
