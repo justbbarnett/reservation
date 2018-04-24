7dc112f8e4f86375db3f0a1064e012697b2ad87c
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-23 19:37:36 -0500

    post method for newReservation

--- app/routing/routes.js
+++ app/routing/routes.js
@@ -4,7 +4,13 @@ var path = require('path');
 var routes = express.Router();
 
 routes.get('', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
-routes.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '../public/tables.html')));
+routes.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '../public/table.html')));
 routes.get('/reservations', (req, res) => res.sendFile(path.join(__dirname, '../public/reservation.html')));
+
+routes.post('/reservations', function(req, res) {
+    let newReservation = req.body;
+    
+    res.json(newReservation);
+} )
 
 module.exports = routes;
